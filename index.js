fccf9ff12097696208425347b4cb78e93da218b9
MursalinDevOps <mursalin1768@gmail.com>
2024-12-10 11:50:25 +0600

    add coffee form data recived

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -17,31 +17,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // receive add coffee form data from the client side
+    app.post("/coffee", async (req, res) => {
+      const addCoffeeFormData = req.body;
+      console.log(addCoffeeFormData);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", async (req, res) => {
+  res.send("ESPRESSO SERVER IS RUNNING...");
+});
 
-
-
-
-app.get('/', async(req, res) => {
-    res.send('ESPRESSO SERVER IS RUNNING...')
-})
-
-app.listen(port, ()=>{
-    console.log(`espresso emporium is running on ${port} port!`)
-})
+app.listen(port, () => {
+  console.log(`espresso emporium is running on ${port} port!`);
+});
